cf64614caf7d1bac58a5550d2c4c5029e02829b3
backer <zm.backer@gmail.com>
2014-07-02 00:47:02 +0800

    fixed a bug when the rails application doesn't use simple_form, there will be an error. the gem doesn't depand on simple_form.

--- lib/enum_help.rb
+++ lib/enum_help.rb
@@ -1,5 +1,14 @@
 require "enum_help/version"
-require "enum_help/simple_form"
+
+simple_form_exist = true
+begin
+  require 'simple_form'
+rescue LoadError
+  simple_form_exist = false
+end
+if simple_form_exist
+  require "enum_help/simple_form"
+end
 require "enum_help/i18n"
 require "enum_help/railtie"
 
