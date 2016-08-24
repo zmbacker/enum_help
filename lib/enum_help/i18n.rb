b9346eb2c8d68b3c52658e4f79152cefde2e4aa8
Yoshiyuki Hirano <yhirano@aiming-inc.com>
2016-08-25 08:59:28 +0900

    Add EnumHelp::Helper::define_collection_i18n_options_method

--- lib/enum_help/i18n.rb
+++ lib/enum_help/i18n.rb
@@ -8,6 +8,7 @@ module EnumHelp
       definitions.each do |name, _|
         Helper.define_attr_i18n_method(self, name)
         Helper.define_collection_i18n_method(self, name)
+        Helper.define_collection_i18n_options_method(self, name)
       end
     end
 
@@ -44,6 +45,20 @@ module EnumHelp
       def #{collection_i18n_method_name}
         collection_array = #{collection_method_name}.collect do |label, _|
           [label, ::EnumHelp::Helper.translate_enum_label(self, :#{attr_name}, label)]
+        end
+        Hash[collection_array].with_indifferent_access
+      end
+      METHOD
+    end
+
+    def self.define_collection_i18n_options_method(klass, attr_name)
+      collection_method_name = "#{attr_name.to_s.pluralize}"
+      collection_i18n_options_method_name = "#{collection_method_name}_i18n_options"
+
+      klass.instance_eval <<-METHOD, __FILE__, __LINE__
+      def #{collection_i18n_options_method_name}
+        collection_array = #{collection_method_name}.collect do |label, value|
+          [::EnumHelp::Helper.translate_enum_label(self, :#{attr_name}, label), value]
         end
         Hash[collection_array].with_indifferent_access
       end
